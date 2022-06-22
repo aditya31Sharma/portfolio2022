24cfcc48d687c86a82e3e4f23df6ec7d12f4c02b
aditya31Sharma <aditya311001sharma@gmail.com>
2022-06-22 22:27:14 +0530

    Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import Footer from "./components/Footer";
 const App = () => {
   return (
     <>
-      {/* <Loader/> */}
+      <Loader/>
       <BrowserRouter>
       <Navigationbar />
         <Routes>
